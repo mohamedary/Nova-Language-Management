903ae89a59d3727f5aaa1563401aea1f015ce2ad
Crayon <darkspear123@outlook.com>
2021-05-30 09:37:01 +0200

    Updated composer.json

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/languages', fn() => \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getSupportedLocales());
-Route::post('/languages', [\Crayon\NovaLanguageEditor\Http\Controllers\LanguageController::class, 'store']);
+Route::post('/languages', [\Crayon\NovaLanguageEditor\Http\Controllers\LanguageController::class, 'store'])->middleware('throttle:10,1');
